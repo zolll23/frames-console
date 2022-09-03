b75acc5105491009d56b1a1456e51266af73f04f
Andrey Pakhomov <andrey.pahomov@gmail.com>
2022-09-04 01:38:32 +0300

    Add example for Text

--- src/VPA/Console/Glyphs/Page.php
+++ src/VPA/Console/Glyphs/Page.php
@@ -14,4 +14,17 @@ class Page extends GlyphBlock
         parent::__construct($globalConfig);
         $this->documentWidth = $this->gc('shell')->getDocumentWidthFromOS();
     }
+
+    public function getHeightByContent(int $endOfPreviousSibling = 0): int
+    {
+        $this->Y = $endOfPreviousSibling;
+        $this->height = 0;
+        $offset = 0;
+        foreach ($this->children as $child) {
+            $height = $child->getHeightByContent($offset);
+            $offset += $height;
+            $this->height += $height;
+        }
+        return $this->height;
+    }
 }
