b2da12edef35320dce50cdbdeff3dba6b4d5a85b
Andrey Pakhomov <andrey.pahomov@gmail.com>
2022-09-10 02:16:06 +0300

    Add component Table1D

---
+++ examples/table1d.php
@@ -0,0 +1,67 @@
+<?php
+
+use VPA\Console\Components\Table1D;
+use VPA\Console\Shell;
+use VPA\Console\TableDisplayMode;
+use VPA\DI\Container;
+use VPA\Console\FrameConsoleConfig;
+use VPA\Console\Glyphs\Page;
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$di = new Container();
+$di->registerContainers([
+    'VPA\Console\FrameConfigInterface' => FrameConsoleConfig::class,
+]);
+
+$shell = new Shell();
+$config = new FrameConsoleConfig($shell);
+
+try {
+    $data = [
+        'Company' => 'Everyone Inc',
+        'Date' => '2002',
+        'Description' => 'You may recognise the first few words as "lorem ipsum dolor sit amet".',
+        'Code 1' => 'XU19299',
+        'Code IPU' => 'UI2929I199',
+        'Serial No' => '19889',
+    ];
+    $table1d = $di->get(Table1D::class);
+    $table1d->setConfig([
+        'columns' => 2,
+        'secondColumnWidth' => 30,
+    ]);
+    $table1d->output($data);
+
+    $table1d = $di->get(Table1D::class);
+    $table1d->setConfig([
+        'secondColumnWidth' => 30,
+    ])->setHeader("Attribute", "Value");
+    $table1d->output($data);
+
+    $table1d = $di->get(Table1D::class);
+    $table1d->setConfig([
+        'columns' => 'auto',
+        'secondColumnMaxWidth' => 30,
+    ])->setHeader("Attribute", "Value");
+    $table1d->output($data);
+
+
+    $table1d = $di->get(Table1D::class);
+    $table1d->setConfig([
+        'type' => TableDisplayMode::Frame,
+        'secondColumnWidth' => 30,
+    ])->setHeader("Attribute", "Value");
+    $table1d->output($data);
+
+    $table1d = $di->get(Table1D::class);
+    $table1d->setConfig([
+        'type' => TableDisplayMode::Frameless,
+        'secondColumnWidth' => 30,
+    ])->setHeader("Attribute", "Value");
+    $table1d->output($data);
+} catch (\Exception $e) {
+    echo $e->getMessage();
+}
+// Padding bottom for demo
+echo str_repeat("\n", 5);
---
+++ src/VPA/Console/Components/Table1D.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace VPA\Console\Components;
+
+use VPA\Console\FrameConfigInterface;
+use VPA\Console\Glyphs\Glyph;
+use VPA\Console\Glyphs\GlyphBlock;
+use VPA\Console\TableDisplayMode;
+
+class Table1D extends GlyphBlock
+{
+    private string $firstColumnTitle;
+    private string $secondColumnTitle;
+    private bool $haveHeader = false;
+    private int|string $firstWidth;
+    private int|string $secondWidth;
+    private int|string $firstMaxWidth;
+    private int|string $secondMaxWidth;
+    private array $data = [];
+    private int $columns = 1;
+    private const PADDING_X = 1;
+
+    public function __construct(protected FrameConfigInterface $globalConfig)
+    {
+        parent::__construct($globalConfig);
+        $this->config = array_merge(
+            parent::getConfig(),
+            [
+                'type' => TableDisplayMode::Slim,
+                'columns' => '1',
+                'firstColumnWidth' => 'auto',
+                'firstColumnMaxWidth' => 'auto',
+                'secondColumnWidth' => 'auto',
+                'secondColumnMaxWidth' => 'auto',
+            ]
+        );
+        $this->documentWidth = $this->gc('shell')->getDocumentWidthFromOS();
+    }
+
+    public function setHeader(string|array $firstTitle, ?string $secondTitle): Glyph
+    {
+        $this->haveHeader = true;
+        if (is_array($firstTitle) && count($firstTitle) == 2) {
+            $this->firstColumnTitle = reset($firstTitle);
+            $this->secondColumnTitle = end($firstTitle);
+            return $this;
+        }
+        $this->firstColumnTitle = $firstTitle ?? '';
+        $this->secondColumnTitle = $secondTitle ?? '';
+        return $this;
+    }
+
+    private function getAutoColumnsNum(): int
+    {
+        $columns = intval($this->__get('columns'));
+        $this->firstWidth = $this->__get('firstColumnWidth');
+        $this->firstMaxWidth = $this->__get('firstColumnMaxWidth');
+        $this->secondWidth = $this->__get('secondColumnWidth');
+        $this->secondMaxWidth = $this->__get('secondColumnMaxWidth');
+        $type = $this->__get('type');
+        $deltaWidth = self::PADDING_X * 4;
+        $deltaWidth += match ($type) {
+            TableDisplayMode::Slim, TableDisplayMode::Frame => 3,
+            default => 0,
+        };
+        if ($columns == 0) {
+            $valueWidth = max(array_map(function ($it) {
+                return strlen($it);
+            }, $this->data));
+            $keyWidth = max(array_map(function ($it) {
+                return strlen($it);
+            }, array_keys($this->data)));
+            $keyRealWidth = min([$keyWidth, $this->firstWidth, $this->firstMaxWidth]);
+            $valueRealWidth = min([$valueWidth, $this->secondWidth, $this->secondMaxWidth]);
+            $width = $keyRealWidth + $valueRealWidth + $deltaWidth;
+            return floor($this->getDocumentWidth() / $width);
+        }
+        return $columns;
+    }
+
+    public function output($data): Glyph
+    {
+        $this->data = $data;
+        $table = $this->addTable();
+        $type = $this->__get('type');
+        $this->columns = $this->getAutoColumnsNum();
+
+        $borderHead = $borderV = $borderH = 0;
+        switch ($type) {
+            case TableDisplayMode::Slim:
+                $borderV = 1;
+                $borderHead = 1;
+                $table->setBorder(1, 1, 1, 1);
+                break;
+            case TableDisplayMode::Frame:
+                $borderV = 1;
+                $borderH = 1;
+                $borderHead = 1;
+                $table->setBorder(1, 1, 1, 0);
+                break;
+        }
+
+        if ($this->haveHeader) {
+            $header = $table->addRow();
+            for ($i = 0; $i < $this->columns; $i++) {
+                $this->addCells($header, $this->firstColumnTitle, $this->secondColumnTitle, $borderHead, $borderV, $i);
+            }
+        }
+        $value = current($data);
+        $key = key($data);
+        do {
+            $row = $table->addRow();
+            for ($i = 0; $i < $this->columns; $i++) {
+                if ($key) {
+                    $this->addCells($row, $key, $value, $borderH, $borderV, $i);
+                    $value = next($data);
+                    $key = key($data);
+                } else {
+                    $this->addCells($row, '', '', $borderH, $borderV, $i);
+                }
+
+            }
+        } while ($value);
+        $this->display();
+        return $this;
+    }
+
+    private function addCells(
+        Glyph $row,
+        string $first,
+        string $second,
+        int $borderBottom = 0,
+        int $borderV = 1,
+        int $column = 1
+    ): array
+    {
+        $firstCell = $row->addCell()
+            ->setPadding(self::PADDING_X, self::PADDING_X, 0, 0)->setBorder(0, $borderV, 0, $borderBottom);
+        $this->firstWidth !== 'auto' && $firstCell = $firstCell->setWidth(intval($this->firstWidth));
+        $firstText = $firstCell->addText();
+        $this->firstMaxWidth != 'auto' && $firstText = $firstText->setConfig(['maxWidth' => $this->firstMaxWidth]);
+        $firstText->setValue($first);
+        $secondCell = $row->addCell()
+            ->setPadding(self::PADDING_X, self::PADDING_X, 0, 0)
+            ->setBorder(0, $this->columns - 1 == $column ? 0 : $borderV, 0, $borderBottom);
+        $this->secondWidth !== 'auto' && $firstCell = $secondCell->setWidth(intval($this->secondWidth));
+        $secondText = $secondCell->addText();
+        $this->secondMaxWidth != 'auto' && $secondText = $secondText->setConfig(['maxWidth' => $this->secondMaxWidth]);
+        $secondText->setValue($second);
+        return [&$firstCell, &$secondCell];
+    }
+}
